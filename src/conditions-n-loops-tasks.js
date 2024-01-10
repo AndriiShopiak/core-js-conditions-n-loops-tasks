952cfd8e649714f1358e6d39ff1d193142ea4564
Andrii Shopiak <andriyshopyak@gmail.com>
2024-01-10 19:46:46 +0200

    feat: add solution for isPalindrome task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -189,8 +189,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let res = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    res += str[i];
+  }
+  return res === str;
 }
 
 /**
